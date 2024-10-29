25614cae60ec6a98fba1a50c77b5b887ce399d08
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-10-29 18:00:29 -0300

    Adiciona o modelo Student

---
+++ models/Student.js
@@ -0,0 +1,15 @@
+// models/Student.js
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  cpf: { type: String, required: true },
+  birthDate: { type: Date, required: true },
+  institution: { type: String, required: true },
+  course: { type: String, required: true },
+  issuer: { type: String, required: true },
+  validity: { type: Date, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+module.exports = mongoose.model("Student", StudentSchema);
