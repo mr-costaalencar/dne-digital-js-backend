699ee3d07fd5e186bb38f8b19122c4310d3f3e09
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-05-17 10:21:32 -0300

    feat: :sparkles: Atualiza a conexão com MongoDB e adiciona configuração de CI/CD

--- config/db.js
+++ config/db.js
@@ -4,10 +4,11 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`{process.env.MONGO_URI}`);
     console.log("MongoDB conectado");
   } catch (err) {
-    console.error(err.message);
+    console.error(`Erro ao conectar ao MongoDB: ${err.message}`);
+    console.error(`URI do MongoDB: ${process.env.MONGO_URI}`);
     process.exit(1);
   }
 };
