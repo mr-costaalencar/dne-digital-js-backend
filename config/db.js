b93004c2b507ea1cbe67dbcaa7f88fff459f2961
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-05-19 20:10:54 -0300

    fix: :bug: Corrige a URI de conexão com o MongoDB para usar MONGODB_URI

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`{process.env.MONGO_URI}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log("MongoDB conectado");
   } catch (err) {
     console.error(`Erro ao conectar ao MongoDB: ${err.message}`);
